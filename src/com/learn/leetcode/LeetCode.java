88a8fa6576caf200bf176047d4d4128d88430039
caokeyu <ckysaltedfish@163.com>
2020-12-23 23:57:27 +0800

    long

---
+++ src/com/learn/leetcode/LeetCode.java
@@ -0,0 +1,259 @@
+/*
+ * Copyright (C) 2011-2020 ShenZhen iBOXCHAIN Information Technology Co.,Ltd.
+ *
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary
+ * information of iBoxChain Company of China.
+ * ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only
+ * in accordance with the terms of the contract agreement
+ * you entered into with iBoxChain inc.
+ *
+ */
+
+package com.learn.leetcode;
+
+/**
+ * Description：
+ *
+ * @author caokeyu
+ * @since 2020/12/22
+ */
+public class LeetCode {
+
+  public static void main(String[] args) {
+//    System.out.println(findMedianSortedArrays(new int[]{1,3,4,5}, new int[]{2}));
+    System.out.println(longestPalindrome2("baa"));
+  }
+
+  public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    int mid = (nums1.length + nums2.length) / 2 + 1;
+    int nn = mid;
+    if (mid == 1) {
+      if (nums1.length == 1) {
+        return nums1[0];
+      } else if (nums2.length == 1) {
+        return nums2[0];
+      }
+      return 0;
+    }
+    int offset1 = 0, offset2 = 0;
+    while (true) {
+      int m = mid / 2 - 1;
+      int m1 = m > 0 ? m + offset1 : 0, m2 = m > 0 ? m + offset2: 0;
+      if (nums1.length <= m1) {
+        if ((nums1.length + nums2.length) % 2 == 0) {
+          return (nums2[nn - nums1.length - 1] + nums2[nn - nums1.length - 2]) / 2.0;
+        } else {
+          return (nums2[nn - nums1.length - 1]) / 2;
+        }
+      }
+      if (nums2.length <= m2) {
+        if ((nums1.length + nums2.length) % 2 == 0) {
+          return (nums1[nn - nums2.length - 1] + nums1[nn - nums2.length - 2]) / 2.0;
+        } else {
+          return (nums1[nn - nums2.length - 1]) / 2;
+        }
+      }
+      if (nums1[m1] > nums2[m2]) {
+        offset2 += m + 1;
+
+      } else if (nums1[m1] < nums2[m2]) {
+        offset1 += m + 1;
+      } else {
+        offset1 += m;
+        offset2 += m;
+      }
+      mid = mid - (mid / 2);
+      if (mid == 1) {
+        if ((nums1.length + nums2.length) % 2 == 0) {
+          return (nums1[m1] +nums2[m2]) / 2.0;
+        } else {
+          if (nums1[m1] < nums2[m2]) {
+            return nums1[m2];
+          }
+          return nums2[m1];
+        }
+      }
+    }
+
+
+
+  }
+
+
+  public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+    int length1 = nums1.length, length2 = nums2.length;
+    int totalLength = length1 + length2;
+    if (totalLength % 2 == 1) {
+      int midIndex = totalLength / 2;
+      double median = getKthElement(nums1, nums2, midIndex + 1);
+      return median;
+    } else {
+      int midIndex1 = totalLength / 2 - 1, midIndex2 = totalLength / 2;
+      double median = (getKthElement(nums1, nums2, midIndex1 + 1) + getKthElement(nums1, nums2, midIndex2 + 1)) / 2.0;
+      return median;
+    }
+  }
+
+  public static int getKthElement(int[] nums1, int[] nums2, int k) {
+    /* 主要思路：要找到第 k (k>1) 小的元素，那么就取 pivot1 = nums1[k/2-1] 和 pivot2 = nums2[k/2-1] 进行比较
+     * 这里的 "/" 表示整除
+     * nums1 中小于等于 pivot1 的元素有 nums1[0 .. k/2-2] 共计 k/2-1 个
+     * nums2 中小于等于 pivot2 的元素有 nums2[0 .. k/2-2] 共计 k/2-1 个
+     * 取 pivot = min(pivot1, pivot2)，两个数组中小于等于 pivot 的元素共计不会超过 (k/2-1) + (k/2-1) <= k-2 个
+     * 这样 pivot 本身最大也只能是第 k-1 小的元素
+     * 如果 pivot = pivot1，那么 nums1[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums1 数组
+     * 如果 pivot = pivot2，那么 nums2[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums2 数组
+     * 由于我们 "删除" 了一些元素（这些元素都比第 k 小的元素要小），因此需要修改 k 的值，减去删除的数的个数
+     */
+
+    int length1 = nums1.length, length2 = nums2.length;
+    int index1 = 0, index2 = 0;
+    int kthElement = 0;
+
+    while (true) {
+      // 边界情况
+      if (index1 == length1) {
+        return nums2[index2 + k - 1];
+      }
+      if (index2 == length2) {
+        return nums1[index1 + k - 1];
+      }
+      if (k == 1) {
+        return Math.min(nums1[index1], nums2[index2]);
+      }
+
+      // 正常情况
+      int half = k / 2;
+      int newIndex1 = Math.min(index1 + half, length1) - 1;
+      int newIndex2 = Math.min(index2 + half, length2) - 1;
+      int pivot1 = nums1[newIndex1], pivot2 = nums2[newIndex2];
+      if (pivot1 <= pivot2) {
+        k -= (newIndex1 - index1 + 1);
+        index1 = newIndex1 + 1;
+      } else {
+        k -= (newIndex2 - index2 + 1);
+        index2 = newIndex2 + 1;
+      }
+    }
+  }
+
+  public static String longestPalindrome(String s) {
+    if (s.length() <= 1) {
+      return s;
+    }
+    int[] temp = new int[128];
+    for (int i = 0; i < temp.length; i++) {
+      temp[i] = -1;
+    }
+    char[] chars = s.toCharArray();
+    int left = 0;
+    int right = 0;
+    int maxLen = 0;
+    boolean isFind = false;
+    String result = "";
+    while (true) {
+      isFind = false;
+      char c = s.charAt(right);
+      char d = s.charAt(left);
+      if (temp[d] > 0) {
+        if (temp[d] != left && temp[d] - left <= maxLen) {
+          left++;
+          continue;
+        }
+      }
+      if (temp[c] < 0) {
+        temp[c] = right;
+        right++;
+      } else {
+        if (chars[left] == c) {
+          isFind = true;
+          if(maxLen < (right - left)) {
+            result = s.substring(left, right + 1);
+            maxLen = right - left;
+          }
+          left++;
+        }
+        temp[c] = right;
+        right++;
+      }
+
+      if (right == s.length() && left != (right - 1) && !isFind) {
+        left++;
+        right = left + 1;
+      }
+      if (right == s.length() && left == (right - 1)) {
+
+        if (result.equals("")) {
+          result = String.valueOf(chars[0]);
+        }
+
+        break;
+      }
+      if (right == s.length() && isFind) {
+        break;
+      }
+    }
+    return result;
+  }
+
+
+
+  public static String longestPalindrome2(String s) {
+    if (s.length() <= 1) {
+      return s;
+    }
+    char[] chars = s.toCharArray();
+    int left = 0;
+    int right = s.length() - 1;
+    int maxLen = 0;
+    boolean flag = false;
+    String result = "";
+    while (right >= left) {
+
+      if(isEqual(chars, left, right)) {
+        if (maxLen < right - left + 1) {
+          maxLen = right - left + 1;
+          result = s.substring(left, right + 1);
+        }
+        left++;
+        right = s.length() - 1;
+
+      } else {
+        right--;
+      }
+
+
+
+      if (maxLen > right - left + 1) {
+        left++;
+        right = s.length() - 1;
+        if (maxLen > right - left + 1) {
+          break;
+        }
+      }
+
+
+
+    }
+
+    if (result.equals("")) {
+      result = String.valueOf(chars[0]);
+    }
+    return result;
+  }
+
+  private static boolean isEqual(char[] chars, int left, int right) {
+    if (chars[left] == chars[right]) {
+      if (left <= right && right - 1 >= 0 && left + 1 < chars.length) {
+        return isEqual(chars, left + 1, right - 1);
+      }
+      return true;
+    }
+    return false;
+  }
+
+
+}
