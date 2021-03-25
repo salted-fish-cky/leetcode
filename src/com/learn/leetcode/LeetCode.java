cca292eb49c802d9491b30ff2dbbb4299245fb6c
caokeyu <caokeyu@iboxpay.com>
2021-03-25 09:51:37 +0800

    二分法

--- src/com/learn/leetcode/LeetCode.java
+++ src/com/learn/leetcode/LeetCode.java
@@ -1316,6 +1316,27 @@ public class LeetCode {
     return resultList;
   }
 
+  /**
+   * 704. 二分查找
+   * @param nums
+   * @param target
+   * @return
+   */
+  public int search2(int[] nums, int target) {
+    int start = 0,end = nums.length - 1, mid = (start + end)/2;
+    while(start < end) {
+      if (nums[mid] > target) {
+        end = mid - 1;
+      } else if (nums[mid] < target) {
+        start = mid + 1;
+      } else {
+        return mid;
+      }
+      mid = (start + end)/2;
+    }
+    return -1;
+  }
+
 
 
   private static ListNode convertNode(ListNode l, ListNode node) {
