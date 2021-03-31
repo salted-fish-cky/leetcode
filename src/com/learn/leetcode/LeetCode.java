575d7ba198ac3999d93ec3a19f90de82b47c7fe5
caokeyu <ckysaltedfish@163.com>
2021-03-31 22:21:03 +0800

    update

--- src/com/learn/leetcode/LeetCode.java
+++ src/com/learn/leetcode/LeetCode.java
@@ -1637,6 +1637,23 @@ public class LeetCode {
     return dp[n];
   }
 
+  /**
+   * 518. 零钱兑换 II
+   * @param amount
+   * @param coins
+   * @return
+   */
+  public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
+    for (int i = 0; i < coins.length; i++) {
+      for (int j = coins[i]; j <= amount; j++) {
+        dp[j] += dp[j - coins[i]];
+      }
+    }
+    return dp[amount];
+  }
+
 
 
   private static ListNode convertNode(ListNode l, ListNode node) {
